a4342e6dd8056f032b48aa068cdd6edb95b18325
Florian Weich <hallo@florianweich.me>
2018-07-09 17:00:12 +0200

    Updated contact form to use own Mail Service API

--- _javascript/scripts.js
+++ _javascript/scripts.js
@@ -7,51 +7,64 @@ var toast_breakpoint_medium = 700;
 */
 
 $.fn.extend({
-    animateCss: function (animationName, afterClass) {
-        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-        $(this).addClass('animated ' + animationName).one(animationEnd, function() {
-            $(this).removeClass('animated ' + animationName).addClass(afterClass);
-        });
-    }
+  animateCss: function(animationName, afterClass) {
+    var animationEnd =
+      'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    $(this)
+      .addClass('animated ' + animationName)
+      .one(animationEnd, function() {
+        $(this)
+          .removeClass('animated ' + animationName)
+          .addClass(afterClass);
+      });
+  }
 });
 
-$( document ).ready(function() {
-
-	/*
+$(document).ready(function() {
+  /*
 		SCROLL NAVIGATION
 	*/
 
-	$('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
+    var target = this.hash;
+    var $target = $(target);
 
-	    if ($target.length === 0) return;
+    if ($target.length === 0) return;
 
-      // Disable scroll ANIMATIONS
-      if (target == "#kontakt") {
-        $target.find("#contactform, .social-list").addClass("in-view visible");
-      }
+    // Disable scroll ANIMATIONS
+    if (target == '#kontakt') {
+      $target.find('#contactform, .social-list').addClass('in-view visible');
+    }
 
-      // Hide mobile nav if shown
-      var $mainNav = $("#main-nav");
-      if ( $mainNav.hasClass("shown") ) {
-    		$mainNav.removeClass("shown").animateCss("fadeOutLeftBig", "hidden-mobile");
-    	}
+    // Hide mobile nav if shown
+    var $mainNav = $('#main-nav');
+    if ($mainNav.hasClass('shown')) {
+      $mainNav
+        .removeClass('shown')
+        .animateCss('fadeOutLeftBig', 'hidden-mobile');
+    }
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 400, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+    $('html, body')
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top
+        },
+        400,
+        'swing',
+        function() {
+          window.location.hash = target;
+        }
+      );
+  });
 
-	/*
+  /*
 		MASONRY
 	*/
 
-	loadMasonry();
+  loadMasonry();
 
   /*
     CARD ANI
@@ -72,21 +85,43 @@ $( document ).ready(function() {
   /**
    * CONTACT FORM SUBMISSION
    */
-  $('#contactform').on('submit', function(e){
+  $('#contactform').on('submit', function(e) {
     e.preventDefault();
 
     // Check if form is valid
-    if ( $(this).parsley().isValid() ) {
-      $.post( $(this).attr('action'), $(this).serialize() ).done(function (result) {
-        if ( result.success == true ) {
-          document.location.href = "/danke/";
-        } else {
-          console.log("Form submission failed!");
-          if ( result.error ) {
-            console.log("Error: " + result.error);
+    if (
+      $(this)
+        .parsley()
+        .isValid()
+    ) {
+      $.ajax({
+        url: 'https://mailservice.florianweich.me/api/sendmail',
+        type: 'POST',
+        headers: {
+          Authorization: 'Bearer 38b446a0d7a343dba61c4135a6c537e6',
+          'Content-Type': 'application/json; charset=utf-8'
+        },
+        contentType: 'application/json',
+        data: JSON.stringify({
+          email: $('#contactform input[name=email]').val(),
+          subject: 'Neue Nachricht vom Kontaktformular',
+          content: $('#contactform textarea[name=message]').val(),
+          name: $('#contactform input[name=name]').val(),
+          privacypolicy:
+            $('#contactform input[name=privacypolicy]').val() == 'on'
+              ? true
+              : false
+        })
+      })
+        .done(function(data, textStatus, jqXHR) {
+          console.log('HTTP Request Succeeded: ' + jqXHR.status);
+          if (data.sent === true) {
+            document.location.href = '/danke/';
           }
-        }
-      });
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          console.log('HTTP Request Failed');
+        });
     }
   });
 
@@ -94,123 +129,127 @@ $( document ).ready(function() {
     SLIDER
   */
 
-  if ( $('#masterslider').length > 0 ) {
+  if ($('#masterslider').length > 0) {
     initMasterSlider();
   }
-
 });
-
 
 /*
 	LOAD REFS MASONRY
 */
 
 function loadMasonry() {
-  $('.references-list').imagesLoaded( function() {
-		$('.references-list').masonry({
-			itemSelector: '.references-list-item'
-		});
+  $('.references-list').imagesLoaded(function() {
+    $('.references-list').masonry({
+      itemSelector: '.references-list-item'
+    });
     if (isMobile.any === false) {
       check_if_in_view();
     } else {
       showRefCards();
     }
-	});
-}
-
-function animateCards() {
-  $('.cards').find('.card').each(function(i) {
-    var delay = i * 300;
-    var $item = $(this);
-    setTimeout(function() {
-      $item.css("opacity", "1").animateCss("bounceInUp", "visible");
-    }, delay);
   });
 }
 
+function animateCards() {
+  $('.cards')
+    .find('.card')
+    .each(function(i) {
+      var delay = i * 300;
+      var $item = $(this);
+      setTimeout(function() {
+        $item.css('opacity', '1').animateCss('bounceInUp', 'visible');
+      }, delay);
+    });
+}
+
 function showCards() {
-  $('.card').addClass("visible");
+  $('.card').addClass('visible');
 }
 
 function showRefCards() {
-  $('.references-list-item').addClass("visible");
+  $('.references-list-item').addClass('visible');
 }
-
 
 /*
   DROPDOWN MENU
 */
 
 $('.dropdown .parent').click(function(e) {
-	e.preventDefault();
+  e.preventDefault();
 });
 
-if ( $( document ).width() > toast_breakpoint_medium) {
-
-	$('.dropdown').mouseenter(function() {
-	  $('.dropdown .dropdown-menu').show();
-	});
+if ($(document).width() > toast_breakpoint_medium) {
+  $('.dropdown').mouseenter(function() {
+    $('.dropdown .dropdown-menu').show();
+  });
 
   $('.dropdown').click(function() {
     $('.dropdown-menu').show();
     event.stopPropagation();
   });
 
-	$('.dropdown-menu').mouseleave(function() {
-	  $(this).hide();
-	});
+  $('.dropdown-menu').mouseleave(function() {
+    $(this).hide();
+  });
 
-	$('html').click(function() {
-	  $('.dropdown-menu').hide();
-	});
+  $('html').click(function() {
+    $('.dropdown-menu').hide();
+  });
 
-	$('.dropdown-menu').click(function(event){
-	  event.stopPropagation();
-	});
-
+  $('.dropdown-menu').click(function(event) {
+    event.stopPropagation();
+  });
 }
 
 /*
   MOBILE MENU
 */
 
-$("#mobile-menu-btn").click(function() {
+$('#mobile-menu-btn').click(function() {
+  var $mainNav = $('#main-nav');
 
-  var $mainNav = $("#main-nav");
+  if ($mainNav.hasClass('animated')) {
+    return;
+  }
 
-	if ( $mainNav.hasClass("animated") ) { return; }
-
-	if ( $mainNav.hasClass("shown") ) {
-		$mainNav.removeClass("shown").animateCss("fadeOutLeftBig", "hidden-mobile");
-	} else {
-		$mainNav.removeClass("hidden-mobile").animateCss("bounceInLeft", "shown");
-	}
-
+  if ($mainNav.hasClass('shown')) {
+    $mainNav.removeClass('shown').animateCss('fadeOutLeftBig', 'hidden-mobile');
+  } else {
+    $mainNav.removeClass('hidden-mobile').animateCss('bounceInLeft', 'shown');
+  }
 });
 
 /*
   ANIMATE COLLECTION ITEMS ON SCROLL
 */
 
-var $animation_elements = $('.collection-item, .references-list-item, .btn-animated');
+var $animation_elements = $(
+  '.collection-item, .references-list-item, .btn-animated'
+);
 var $window = $(window);
 
 function check_if_in_view() {
   var window_height = $window.height();
   var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
+  var window_bottom_position = window_top_position + window_height;
 
   $.each($animation_elements, function() {
     var $element = $(this);
     var element_height = $element.outerHeight();
     var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
+    var element_bottom_position = element_top_position + element_height;
 
     //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position) &&
-        ($element.hasClass("in-view") === false)) {
-          $element.css("opacity", "1").addClass("in-view").animateCss("slideInUp", "visible");
+    if (
+      element_bottom_position >= window_top_position &&
+      element_top_position <= window_bottom_position &&
+      $element.hasClass('in-view') === false
+    ) {
+      $element
+        .css('opacity', '1')
+        .addClass('in-view')
+        .animateCss('slideInUp', 'visible');
     }
   });
 }
@@ -225,28 +264,31 @@ if (isMobile.any === false) {
 */
 
 Parsley.addMessages('de', {
-  defaultMessage: "Die Eingabe scheint nicht korrekt zu sein.",
+  defaultMessage: 'Die Eingabe scheint nicht korrekt zu sein.',
   type: {
-    email:        "Gib bitte eine gültige E-Mail-Adresse ein.",
-    url:          "Die Eingabe muss eine gültige URL sein.",
-    number:       "Die Eingabe muss eine Zahl sein.",
-    integer:      "Die Eingabe muss eine Zahl sein.",
-    digits:       "Die Eingabe darf nur Ziffern enthalten.",
-    alphanum:     "Die Eingabe muss alphanumerisch sein."
+    email: 'Gib bitte eine gültige E-Mail-Adresse ein.',
+    url: 'Die Eingabe muss eine gültige URL sein.',
+    number: 'Die Eingabe muss eine Zahl sein.',
+    integer: 'Die Eingabe muss eine Zahl sein.',
+    digits: 'Die Eingabe darf nur Ziffern enthalten.',
+    alphanum: 'Die Eingabe muss alphanumerisch sein.'
   },
-  notblank:       "Die Eingabe darf nicht leer sein.",
-  required:       "Das hier ist ein Pflichtfeld.",
-  pattern:        "Die Eingabe scheint ungültig zu sein.",
-  min:            "Die Eingabe muss größer oder gleich %s sein.",
-  max:            "Die Eingabe muss kleiner oder gleich %s sein.",
-  range:          "Die Eingabe muss zwischen %s und %s liegen.",
-  minlength:      "Die Eingabe ist zu kurz. Es müssen mindestens %s Zeichen eingegeben werden.",
-  maxlength:      "Die Eingabe ist zu lang. Es dürfen höchstens %s Zeichen eingegeben werden.",
-  length:         "Die Länge der Eingabe ist ungültig. Es müssen zwischen %s und %s Zeichen eingegeben werden.",
-  mincheck:       "Wählen Sie mindestens %s Angaben aus.",
-  maxcheck:       "Wählen Sie maximal %s Angaben aus.",
-  check:          "Wählen Sie zwischen %s und %s Angaben.",
-  equalto:        "Dieses Feld muss dem anderen entsprechen."
+  notblank: 'Die Eingabe darf nicht leer sein.',
+  required: 'Das hier ist ein Pflichtfeld.',
+  pattern: 'Die Eingabe scheint ungültig zu sein.',
+  min: 'Die Eingabe muss größer oder gleich %s sein.',
+  max: 'Die Eingabe muss kleiner oder gleich %s sein.',
+  range: 'Die Eingabe muss zwischen %s und %s liegen.',
+  minlength:
+    'Die Eingabe ist zu kurz. Es müssen mindestens %s Zeichen eingegeben werden.',
+  maxlength:
+    'Die Eingabe ist zu lang. Es dürfen höchstens %s Zeichen eingegeben werden.',
+  length:
+    'Die Länge der Eingabe ist ungültig. Es müssen zwischen %s und %s Zeichen eingegeben werden.',
+  mincheck: 'Wählen Sie mindestens %s Angaben aus.',
+  maxcheck: 'Wählen Sie maximal %s Angaben aus.',
+  check: 'Wählen Sie zwischen %s und %s Angaben.',
+  equalto: 'Dieses Feld muss dem anderen entsprechen.'
 });
 
 Parsley.setLocale('de');
@@ -256,23 +298,25 @@ Parsley.setLocale('de');
 */
 
 function initMasterSlider() {
-  $('#masterslider').masterslider({
-    width: 750,
-    height: 480,
-    space: 0,
-    loop:true,
-    autoplay:true,
-    view:'wave',
-    layout:'partialview',
-    startOnAppear:true,
-    controls: {
-      arrows: { autohide:true }
-    }
-  }).on(MSSliderEvent.CHANGE_START, function(e){
-    var current_slide = $('#masterslider').masterslider('index');
-    $('.slider-nav li').removeClass("active");
-    $('li[data-target-slide="'+ current_slide +'"]').addClass("active");
-  });
+  $('#masterslider')
+    .masterslider({
+      width: 750,
+      height: 480,
+      space: 0,
+      loop: true,
+      autoplay: true,
+      view: 'wave',
+      layout: 'partialview',
+      startOnAppear: true,
+      controls: {
+        arrows: { autohide: true }
+      }
+    })
+    .on(MSSliderEvent.CHANGE_START, function(e) {
+      var current_slide = $('#masterslider').masterslider('index');
+      $('.slider-nav li').removeClass('active');
+      $('li[data-target-slide="' + current_slide + '"]').addClass('active');
+    });
 }
 
 $('.slider-nav li').click(function() {
@@ -283,7 +327,7 @@ $('.slider-nav li').click(function() {
   MY AGE CALC
 */
 function calculateAge() {
-  var birthday = moment("1986-10-09");
+  var birthday = moment('1986-10-09');
   var age = birthday.diff(moment(), 'years') * -1;
-  $('.myage').text(age + " Jahre");
+  $('.myage').text(age + ' Jahre');
 }
